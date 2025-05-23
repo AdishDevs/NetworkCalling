869d915676e9e15e38ab92ca95848c680e2f6714
AdishDevs <adishka@aeosys.com>
2025-05-23 18:52:06 +0530

    Initial Commit

---
+++ NetworkCalling/NetworkCallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallingApp.swift
+//  NetworkCalling
+//
+//  Created by Admin on 2025-05-23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
